43708abd8bbed7a87beb2ffc53d069b58966f9b6
Belén LM <113479814+bellmed22@users.noreply.github.com>
2022-11-11 14:07:55 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function dark() {
+    var element = document.body;
+    element.classList.toggle("dark");
+    var element = document.getElementsByClassName('card');
+        element.style.backgroundColor = "#353330";
+    }
+
+function copy() {
+    alert("216 Sabores - Ciudad de Rosario (Santa Fe) | Whatsapp: *54 341 00 00 00 | 216sabores@mail.com");
+    }
